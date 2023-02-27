d1479a918bebb586ecca374d635030feaaf22c41
Davi Miguel <davi.miguel2020@gmail.com>
2023-02-26 23:21:12 -0300

    básico sobre f string

---
+++ Aula01 - fString/f_String.py
@@ -0,0 +1,11 @@
+# f String
+altura =  1.85
+peso = 95
+nome = 'Davi Miguel'
+imc = peso / altura ** 2
+
+linha_1 = f'{nome} tem {altura:.2f}m de altura, pesa {peso} quilos e seu imc é de {imc:.2f}'
+
+
+print(linha_1)
+
