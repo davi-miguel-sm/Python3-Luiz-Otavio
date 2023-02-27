6cb80c899665d940cfe5e714ab1a0a8f4e261720
Davi Miguel <davi.miguel2020@gmail.com>
2023-02-26 23:27:45 -0300

    metodo format básico

--- Aula01 - fString/f_String.py
+++ Aula01 - fString/f_String.py
@@ -9,3 +9,9 @@ linha_1 = f'{nome} tem {altura:.2f}m de altura, pesa {peso} quilos e seu imc é 
 
 print(linha_1)
 
+# format
+a = 'Alfa 1 2'
+b = 22450.450
+c = True
+formato = 'a={nome1}, b={nome3}, c={nome2:,.2f} '.format(nome1 = a,nome2 = b,nome3 = c)
+print(formato)
