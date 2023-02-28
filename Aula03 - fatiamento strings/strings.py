d1bfde22270d92f844f6e4f817c6b40b3f629b0c
Davi Miguel <davi.miguel2020@gmail.com>
2023-02-27 23:55:08 -0300

    Fatiamento de strings e len

---
+++ Aula03 - fatiamento strings/strings.py
@@ -0,0 +1,9 @@
+# Fatiamento
+# [indice:final-1:passo]
+
+variavel = 'Olá Mundo'
+nome = 'Davi Miguel'
+
+print(variavel[4::2])
+print(nome[::-1]) #inversão rápida de strings
+print(len(variavel[4::2]))
