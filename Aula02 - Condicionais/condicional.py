95516f8e2881d7b42870e9f743cdbedf29800792
Davi Miguel <davi.miguel2020@gmail.com>
2023-02-27 23:27:10 -0300

    Blocos de código e debugger do VsCode finalizado

---
+++ Aula02 - Condicionais/condicional.py
@@ -0,0 +1,13 @@
+nota1 =10
+nota2 = 7
+nota3 = 8
+
+entrada = input('Você quer entrar ou sair?')
+
+if entrada =='entrar':
+    print('Você entrou no sistema!')
+elif entrada =='sair':
+    print('Você saiu do sistema')
+else:
+    print('Você não deve ficar em cima do muro!')
+
