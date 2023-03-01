3fad92521ae6cf31942496c651fce156410a1d86
Davi Miguel <davi.miguel2020@gmail.com>
2023-02-28 23:52:26 -0300

    Introdução ao try except finalizada

--- Aula04 - Try e Except/tryExcept.py
+++ Aula04 - Try e Except/tryExcept.py
@@ -3,11 +3,17 @@
 
 numero_str = input('Digite um número para ser dobrado:')
 
-if numero_str.isdigit():
+# if numero_str.isdigit():
+#     numero_float = float(numero_str)
+#     print(f'O dobro de {numero_str} é {numero_float *2}')
+# else:
+#     print('Isso não é um número.')
+
+try:
+    print('STR:', numero_str)
     numero_float = float(numero_str)
+    print('FLOAT:', numero_float)
     print(f'O dobro de {numero_str} é {numero_float *2}')
-else:
+except:
     print('Isso não é um número.')
-
-
 
